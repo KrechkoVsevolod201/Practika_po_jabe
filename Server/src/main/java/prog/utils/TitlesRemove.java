ae1758720a626a804c9faea745def36a2ba510e1
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2022-12-25 16:35:44 +0400

    Поправил удаление book_title

--- Server/src/main/java/prog/utils/TitlesRemove.java
+++ Server/src/main/java/prog/utils/TitlesRemove.java
@@ -7,7 +7,9 @@ import java.sql.PreparedStatement;
 
 public class TitlesRemove implements CommandExecutor {
 
-    private static final String SQL_DELETE = "delete from titles where book_id = ?";
+    private static final String SQL_DELETE_TITLES = "delete from titles where book_id = ?";
+    private static final String SQL_DELETE_BOOKS = "delete from books where book_id = ?";
+
 
     @Override
     public int execute(String text) {
@@ -22,10 +24,13 @@ public class TitlesRemove implements CommandExecutor {
     @SneakyThrows
     public static int removeTitle(String text) {
         String[] words = text.split(" ");
-        int set = 0;
-        PreparedStatement stat = ApplicationDataSource.getConnection().prepareStatement(SQL_DELETE);
-        stat.setInt(1, Integer.parseInt(words[1]));
-        set = stat.executeUpdate();
-        return set;
+        int setTitles, setBooks  = 0;
+        PreparedStatement statTitles = ApplicationDataSource.getConnection().prepareStatement(SQL_DELETE_TITLES);
+        PreparedStatement statBooks = ApplicationDataSource.getConnection().prepareStatement(SQL_DELETE_BOOKS);
+        statBooks.setInt(1, Integer.parseInt(words[1]));
+        setBooks = statBooks.executeUpdate();
+        statTitles.setInt(1, Integer.parseInt(words[1]));
+        setTitles = statTitles.executeUpdate();
+        return setTitles + setBooks;
     }
 }
